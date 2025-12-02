ca9b6c9932399576a062b26e70d990162824fb1d
jot4jj <jsilvafreitas25@gmail.com>
2025-12-02 15:16:10 -0300

    feat(ui): install Vue Bits split text components and add in hero section component

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,28 +2,52 @@
 import Button from "./ui/button/Button.vue";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Github, Linkedin } from "lucide-vue-next";
+import SplitText from "@/animations/SplitText.vue";
+
 </script>
 
 <template>
-    <section>
-        <Avatar class="w-48 h-48 md:w-64 md:h-64">
+    <section class="min-h-screen w-2/3 flex place-items-center gap-10">
+
+        <Avatar class=" md:w-100 md:h-100">
             <AvatarImage src="https://avatars.githubusercontent.com/u/153793737?v=4"/>
         </Avatar>
-        <div class="space-y-4">
-            <h2 class="text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Olá, me chamo <span class="text-primary">Joao Vitor</span>
+
+        <div class="space-y-6">
+
+            <p>Desenvolvedor Front-end</p>
+
+            <h2 class="text-5xl font-extrabold tracking-tight lg:text-5xl">
+                <SplitText
+                    text="Olá, eu me chamo João Vitor"
+                    class-name="text-5xl font-extrabold"
+                    :delay="80"
+                    :duration="0.2"
+                    ease="power3.out"
+                    split-type="chars"
+                    :from="{ opacity: 0, y: 40 }"
+                    :to="{ opacity: 1, y: 0 }"
+                    :threshold="0.1"
+                    root-margin="-100px"
+                    text-align="center"
+                    @animation-complete="handleAnimationComplete"
+                />
             </h2>
-            <p class="text-xl text-muted-foreground">
-                Desenvolvedor Web Full Stack com paixão por Vue.js, interfaces de usuário incríveis e código limpo. Construindo experiências digitais que importam.
+
+            <p class="text-2xl text-muted-foreground">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fugiat illum tempora enim aperiam sapiente ducimus? Officiis similique, architecto dolorem nihil ipsum numquam cum exercitationem sint mollitia? Aliquam, alias minima.
             </p>
-            <div class="flex justify-center md:justify-start gap-4 mt-6">
-                <a href="https://github.com/jot4jj" target="_blank">
-                    <Github />
-                </a>
-                <a href="">
-                    <Linkedin />
-                </a>
+
+            <div class="flex w-full justify-around">
+                <div>
+                    <a href="https://github.com/jot4jj" target="_blank"> <Github /> </a>
+                </div>
+                
+                <div>
+                    <a href="https://www.linkedin.com/in/jo%C3%A3o-vitor-silva-freitas-b8a13538b/" target="_blank"> <Linkedin /></a>
+                </div>
             </div>
+
         </div>
     </section>
 </template>
