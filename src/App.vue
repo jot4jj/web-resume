6e052c53f029d5c7ea57168a4565ed231e24a02a
jot4jj <jsilvafreitas25@gmail.com>
2025-12-02 09:29:30 -0300

    feat(app): add dark/light theme logic and navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,90 @@
 <script setup lang="ts">
-import Button from './components/ui/button/Button.vue';
+import { ref, onMounted } from "vue";
+import Button from "./components/ui/button/Button.vue";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuContent,
+  NavigationMenuTrigger,
+  NavigationMenuLink
+} from "@/components/ui/navigation-menu";
+
+import ThemeToggle from "./components/ThemeToggle.vue";
+import HeroSection from "./components/HeroSection.vue";
+// import ExperienceTimeLine from "./components/ExperienceTimeline.vue";
+// import ProjectCard from "./components/ProjectCard.vue";
+
+const isDarkMode = ref(false);
+
+onMounted(() => {
+  if (localStorage.theme == "dark") {
+    document.documentElement.classList.add("dark");
+    isDarkMode.value = true;
+  } else {
+    document.documentElement.classList.remove("dark");
+    isDarkMode.value = false;
+  }
+});
+
+const toggleMode = () => {
+  isDarkMode.value = !isDarkMode.value;
+  if (isDarkMode.value) {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+};
 </script>
 
 <template>
-  <Button>Massa demais</Button>
+  <div class="min-h-screen">
+    <NavigationMenu>
+      <NavigationMenuList>
+
+        <NavigationMenuItem>
+          <Button variant="ghost" class="mr-2">Sobre Mim</Button>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <Button variant="ghost" class="mr-2">Experiencias</Button>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>
+            <Button variant="ghost" class="mr-2">Projetos</Button>
+          </NavigationMenuTrigger>
+
+          <NavigationMenuContent>
+
+            <NavigationMenuLink>
+              <a href="#p1"><p>adjkksdjkasdja</p></a>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink>
+              <a href="#p2"><p>adjkksdjkasdja</p></a>
+            </NavigationMenuLink>
+
+            <NavigationMenuLink>
+              <a href="#p3"><p>adjkksdjkasdja</p></a>
+            </NavigationMenuLink>
+
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <ThemeToggle @toggle="toggleMode" :isDarkMode="isDarkMode">Trocar tema</ThemeToggle>
+        </NavigationMenuItem>
+
+      </NavigationMenuList>
+    </NavigationMenu>
+
+    <main class="container mx-auto px-4 py-8 space-y-16">
+      <HeroSection />
+      <ExperienceTimeLine />
+      <ProjectCard />
+    </main>
+  </div>
 </template>
