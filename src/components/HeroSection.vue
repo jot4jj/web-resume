ff3d90759a55c79da3374ffa0f1eae683b4ad475
jot4jj <jsilvafreitas25@gmail.com>
2025-12-09 11:58:24 -0300

    style: replace Avatar component with ProfileCard and change the description

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,49 +1,48 @@
 <script setup>
 import Button from "./ui/button/Button.vue";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Github, Linkedin } from "lucide-vue-next";
 import SplitText from "@/animations/SplitText.vue";
+import ProfileCard from "@/animations/ProfileCard.vue";
+import { Globe, Github, Linkedin } from "lucide-vue-next";
 </script>
 
 <template>
-    <section class="min-h-screen w-4/5 flex place-items-center gap-10 z-50">
-
-        <Avatar class=" md:w-100 md:h-100">
-            <AvatarImage src="https://avatars.githubusercontent.com/u/153793737?v=4"/>
-        </Avatar>
-
-        <div class="space-y-6">
-
-            <p class="relative">Desenvolvedor Front-end</p>
-
-            <h2 class="text-5xl font-extrabold tracking-tight">
-                <SplitText
-                    text="Olá, eu me chamo João Vitor"
-                    class-name="text-5xl font-extrabold"
-                    :delay="30"
-                    :duration="0.2"
-                    ease="power3.out"
-                    split-type="chars"
-                    :from="{ opacity: 0, y: 40 }"
-                    :to="{ opacity: 1, y: 0 }"
-                    :threshold="0.1"
-                    root-margin="-100px"
-                    text-align="left"
+    <section class="min-h-screen w-full flex flex-col place-items-center z-50 xl:flex-row lg:w-3/5">
+        <div class="space-y-6 gap-10 flex flex-col place-items-center lg:flex-row">
+            <div class="my-10 lg:my-0">
+                <ProfileCard
+                    name="João Vitor S."
+                    title="Desenvolvedor Front-end"
+                    avatar-url="./img/profile.png"
+                    :show-user-info="false"
+                    :show-behind-gradient="true"
+                    :enable-tilt="true"
                 />
-            </h2>
+            </div>
 
-            <p class="text-2xl text-muted-foreground text-justify relative">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem fugiat illum tempora enim aperiam sapiente ducimus? Officiis similique, architecto dolorem nihil ipsum numquam cum exercitationem sint mollitia? Aliquam, alias minima.
-            </p>
+            <div class="flex flex-col gap-10 h-full justify-between">
+                <div>
+                    <p class="text-xl md:text-2xl text-justify relative h-full">
+                        Sou João Vitor, desenvolvedor Front-end apaixonado por transformar ideias em interfaces modernas, funcionais e bem estruturadas. Desde 2022 estudo programação e, ao longo da minha formação em Desenvolvimento de Sistemas, venho me especializando em desenvolvimento web, criando projetos que unem boa estética, desempenho e experiência do usuário.
+                    </p>
+                </div>
 
-            <div class="flex w-full justify-around relative">
-                <div>
-                    <a href="https://github.com/jot4jj" target="_blank" > <Github size="50" /> </a>
+                <div class="flex w-full justify-between relative gap-10">
+
+                    <div class="w-full">
+                        <a href="https://github.com/jot4jj" target="_blank" >
+                            <Button class="cursor-target w-full"> <Github /></Button>
+                        </a>
+                    </div>
+                    
+                    <div class="w-full">
+                        <a href="https://www.linkedin.com/in/jo%C3%A3o-vitor-silva-freitas-b8a13538b/" target="_blank">
+                            <Button class="cursor-target w-full"> <Linkedin /></Button>
+                        </a>
+                    </div>
+
                 </div>
-                
-                <div>
-                    <a href="https://www.linkedin.com/in/jo%C3%A3o-vitor-silva-freitas-b8a13538b/" target="_blank"> <Linkedin size="50" /></a>
-                </div>
+
             </div>
 
         </div>
