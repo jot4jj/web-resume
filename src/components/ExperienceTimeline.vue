b566549b0155bc868e4075f3d1a8443b18738af4
jot4jj <jsilvafreitas25@gmail.com>
2025-12-09 12:03:34 -0300

    feat(ui): replace ScrollFloat with GradientText, add SpotlightCard component and complete journey object

--- src/components/ExperienceTimeline.vue
+++ src/components/ExperienceTimeline.vue
@@ -6,20 +6,33 @@ import {
     CardHeader,
     CardTitle
 } from './ui/card';
-import ScrollFloat from '@/animations/ScrollFloat.vue';
+import GradientText from '@/animations/GradientText.vue';
+import SpotlightCard from '@/animations/SpotlightCard.vue';
 
 const journey = [
     {
-        title: 'Estágio em Desenvolvimento de Sistemas',
+        title: 'Primeiro Contato com Programação',
         location: 'EEEP Jaime da Cunha Rebouças',
         period: 'Agosto - Dezembro',
-        description: 'Estágio com foco em aprendizado, aplicando conhecimentos principalmente de Vue.js, Tailwind CSS'
+        description: 'Meu primeiro contato com programação aconteceu em 2022, quando tive minhas primeiras experiências com HTML e CSS. A partir desse momento, passei a me interessar cada vez mais pela área e decidi ingressar no curso técnico de Desenvolvimento de Sistemas para aprofundar meus conhecimentos.'
     },
     {
-        title: 'Estágio em Desenvolvimento de Sistemas',
+        title: 'Curso técnico',
         location: 'EEEP Jaime da Cunha Rebouças',
         period: 'Agosto - Dezembro',
-        description: 'Estágio com foco em aprendizado, aplicando conhecimentos principalmente de Vue.js, Tailwind CSS'
+        description: 'Ao iniciar o curso, fui introduzido aos fundamentos da lógica de programação e, em seguida, estudei minha primeira linguagem: Python. No segundo semestre de 2024, começamos a explorar Programação Web e Programação Orientada a Objetos, ampliando minha base técnica e meu entendimento sobre desenvolvimento de software.'
+    },
+    {
+        title: 'Aprofundamento',
+        location: 'EEEP Jaime da Cunha Rebouças',
+        period: 'Agosto - Dezembro',
+        description: 'Entre o segundo semestre de 2024 e meados de 2025, busquei expandir ainda mais meu conhecimento por meio de cursos externos. Foi nesse período que tive contato com minha primeira linguagem de programação voltada ao desenvolvimento web: JavaScript. Esse aprofundamento me motivou a estudar de forma contínua e começar a construir projetos mais sólidos.'
+    },
+    {
+        title: 'Estágio e primeiros projetos',
+        location: 'EEEP Jaime da Cunha Rebouças',
+        period: 'Agosto - Dezembro',
+        description: 'No segundo semestre de 2025, iniciei meu estágio supervisionado obrigatório, oferecido pela EEEP. Durante essa experiência, consegui consolidar tudo o que havia aprendido até então e aplicar na prática, desenvolvendo novos projetos e adquirindo diversas habilidades adicionais, tanto técnicas quanto profissionais.'
     },
 ]
 
@@ -27,25 +40,29 @@ const journey = [
 
 <template>
 <section>
-    <ScrollFloat
-        children="Minha jornada de aprendizado"
-        :animation-duration="2"
-        text-class-name="tracking-wide"
+    <GradientText
+        text="Minha jornada de aprendizado"
+        :colors="['#fff', '#5C10DB', '#fff']"
+        :animation-speed="8"
+        :show-border="false"
+        class-name="flex p-4 text-4xl text-primary font-extrabold"
     />
 
-    <div class="flex justify-evenly gap-10">
+    <div class="justify-evenly gap-10 grid grid-cols-1 lg:grid-cols-2">
         <div v-for="(item, index) in journey">
-            <Card>
+            <SpotlightCard class="p-4 ">
+
                 <CardHeader>
                     <CardTitle>
                         {{ item.title }}
                     </CardTitle>
                 </CardHeader>
 
-                <CardDescription class="px-4 ">
-                    {{ item.location }} || {{ item.description }}
+                <CardDescription>
+                    {{ item.description }}
                 </CardDescription>
-            </Card>
+
+            </SpotlightCard>
         </div>
     </div>
 </section>
