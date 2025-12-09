0dc7d8509c6e644ff010b2d347cef13b974220f9
jot4jj <jsilvafreitas25@gmail.com>
2025-12-09 11:32:02 -0300

    feat(ui): add AnimatedContent to AppVue

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,49 @@ import HeroSection from "./components/HeroSection.vue";
 import ExperienceTimeline from "./components/ExperienceTimeline.vue";
 import ProjectCard from "./components/ProjectCard.vue";
 import Navigation from "./components/Navigation.vue";
-import Plasma from "./animations/Plasma.vue";
-
+import StackBar from "./components/StackBar.vue";
+import AnimatedContent from "./animations/AnimatedContent.vue";
+import LightRays from "./animations/LightRays.vue";
 </script>
 
 <template>
-  <div class="w-full h-full z-0 absolute">
-    <Plasma
-      color="#FFFFFF"
-      :speed="1"
-      direction="forward"
-      :scale="2"
-      :opacity="0.5"
-      :mouseInteractive="false"
+  <AnimatedContent
+    :distance="160"
+    direction="vertical"
+    :reverse="true"
+    :duration="1"
+    ease="power3.out"
+    :initial-opacity="0"
+    :animate-opacity="true"
+    :scale="1"
+    :threshold="0.1"
+    :delay="0"
+  >
+    <div class="w-full h-full z-0 absolute">
+    <LightRays
+      rays-origin="top-center"
+      rays-color="#5c10db"
+      :rays-speed="1.5"
+      :light-spread="2"
+      :ray-length="1.2"
+      :follow-mouse="true"
+      :mouse-influence="0.1"
+      :noise-amount="0.1"
+      :distortion="0.05"
     />
-  </div>
-  <div class="min-h-screen flex flex-col items-center z-50">
-    <div class="my-4">
-      <Navigation/>
-    </div> 
-    <main class="container justify-center place-items-center mx-auto px-4 py-8 space-y-16">
-      <HeroSection class="z-50" />
-      <ExperienceTimeline class="z-50" />
-      <ProjectCard class="z-50"/>
-    </main>  
-  </div>
+    </div>
+    <div class="min-h-screen flex flex-col items-center z-50">
+      <div class="my-4">
+        <Navigation />
+      </div>
+      <main
+        class="container justify-center place-items-center mx-auto px-4 py-8 space-y-16 w-3/4"
+      >
+        <HeroSection id="hero" />
+        <ExperienceTimeline id="experience" />
+        <StackBar id="stack" />
+        <ProjectCard id="projects" />
+      </main>
+    </div>
+  </AnimatedContent>
 </template>
